6dca98feec5b62d91e080c021cfd3c6dd487d0b4
rroskam <raiderrobert@gmail.com>
2024-04-29 16:10:38 -0400

    tailwind installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  modules: ['@nuxt/content'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+}
+)
